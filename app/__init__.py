3695835773a7aeac58f195c2bd9fdc5e7d8edeb9
memorisanka <j.krawczyk87@gmail.com>
2022-10-16 16:56:15 +0200

    fix(login): changes in login.user_loader

--- app/__init__.py
+++ app/__init__.py
@@ -3,38 +3,35 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from models import User
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.debug = True
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.init_app(app)
+login.login_view = 'login'
+
+
+
+@login.user_loader
+def load_user(user_id):
+    return None
+
+
+from app.routes import index_blueprint, login_blueprint, logout_blueprint, register_blueprint, user_blueprint
+from app import models
+
+
+app.register_blueprint(index_blueprint)
+app.register_blueprint(login_blueprint)
+app.register_blueprint(logout_blueprint)
+app.register_blueprint(register_blueprint)
+app.register_blueprint(user_blueprint)
 
 
 if __name__ == "__main__":
-
-    app = Flask(__name__)
-    app.config.from_object(Config)
     app.debug = True
-    db = SQLAlchemy(app)
-    migrate = Migrate(app, db)
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
-
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-    from app.routes import index_blueprint, login_blueprint, logout_blueprint, register_blueprint, user_blueprint
-    from app import models
-
-
-    app.register_blueprint(index_blueprint)
-    app.register_blueprint(login_blueprint)
-    app.register_blueprint(logout_blueprint)
-    app.register_blueprint(register_blueprint)
-    app.register_blueprint(user_blueprint)
-
     app.run()
-
-
-# if __name__ == "__main__":
-#     app.debug = True
-#     app.run()
--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(UserMixin, db.Model):
